febe447a4aa4e5c2b3636e02af19418a5cb66538
Eduardo Carmo <eduardocarmo72@gmail.com>
2025-01-19 15:33:13 -0300

    finalização do projeto de ordem de livros

--- src/components/template/Pagina.svelte
+++ src/components/template/Pagina.svelte
@@ -9,7 +9,7 @@
     bg-black text-white"
 >
     <Cabecalho />
-    <div class="flex flex-1 max-w-7xl mx-auto w-full p-5">
+    <div class="flex flex-1 gap-5 max-w-7xl mx-auto w-full p-5">
         <main class="flex-1">
             <slot></slot>
         </main>
